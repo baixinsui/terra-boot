079f81abd5056f239d5d5d877cc36ba5c97a9665
baixinsui <baixinsui@outlook.com>
2025-03-05 11:44:17 +0800

    Add API to batch query service order results

--- src/main/java/org/eclipse/xpanse/terra/boot/api/controllers/TerraBootTaskResultApi.java
+++ src/main/java/org/eclipse/xpanse/terra/boot/api/controllers/TerraBootTaskResultApi.java
@@ -9,7 +9,12 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.xpanse.terra.boot.models.response.ReFetchResult;
+import org.eclipse.xpanse.terra.boot.models.response.ReFetchState;
 import org.eclipse.xpanse.terra.boot.models.response.TerraformResult;
 import org.eclipse.xpanse.terra.boot.terraform.service.TerraformResultPersistenceManage;
 import org.springframework.http.HttpStatus;
@@ -18,6 +23,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +38,12 @@ public class TerraBootTaskResultApi {
 
     @Resource private TerraformResultPersistenceManage terraformResultPersistenceManage;
 
+    /**
+     * Fetch the stored terraform result.
+     *
+     * @param requestId id of the request
+     * @return terraform result
+     */
     @Tag(
             name = "RetrieveTerraformResult",
             description = "APIs for manage the task form terra-boot.")
@@ -43,7 +56,51 @@ public class TerraBootTaskResultApi {
     public ResponseEntity<TerraformResult> getStoredTaskResultByRequestId(
             @Parameter(name = "requestId", description = "id of the request")
                     @PathVariable("requestId")
-                    String requestId) {
+                    UUID requestId) {
         return terraformResultPersistenceManage.retrieveTerraformResultByRequestId(requestId);
+    }
+
+    /**
+     * Batch retrieve stored terraform results.
+     *
+     * @param requestIds list of requestIds
+     * @return list of reFetchResults
+     */
+    @Tag(name = "RetrieveOpenTofuResult", description = "APIs for manage the task form tofu-maker.")
+    @Operation(description = "Batch retrieve stored openTofu results")
+    @PostMapping(value = "/results/batch", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public List<ReFetchResult> getBatchTaskResults(
+            @Parameter(description = "List of request IDs") @RequestBody List<UUID> requestIds) {
+        List<ReFetchResult> reFetchResults = new ArrayList<>();
+        requestIds.forEach(
+                requestId -> {
+                    try {
+                        ResponseEntity<TerraformResult> result =
+                                terraformResultPersistenceManage.retrieveTerraformResultByRequestId(
+                                        requestId);
+                        if (result.getStatusCode().is2xxSuccessful()) {
+                            reFetchResults.add(
+                                    ReFetchResult.builder()
+                                            .terraformResult(result.getBody())
+                                            .state(ReFetchState.OK)
+                                            .build());
+                        } else {
+                            reFetchResults.add(
+                                    ReFetchResult.builder()
+                                            .state(
+                                                    ReFetchState
+                                                            .RESULT_ALREADY_RETURNED_OR_REQUEST_ID_INVALID)
+                                            .build());
+                        }
+                    } catch (Exception e) {
+                        reFetchResults.add(
+                                ReFetchResult.builder()
+                                        .state(
+                                                ReFetchState
+                                                        .RESULT_ALREADY_RETURNED_OR_REQUEST_ID_INVALID)
+                                        .build());
+                    }
+                });
+        return reFetchResults;
     }
 }
